017a0ccbb81168da6a4005906f59c708e8bc9db1
Olivier Favre <olivier@yakaz.com>
2013-03-27 16:18:32 +0100

    Reload script: note all inconsistencies
    If a single node has a desired value more recent and different that the
    cluster value, only this node will report the local inconsistency.
    Other nodes' desired value can be more recent but identical to the
    cluster value.
    In such case, multiple nodes updated their file (with regard to the
    cluster settings timestamp), but don't agree in the desired value.
    The script now report such case.
    
    Also print source and inconsistencies of effective setting.

--- src/main/python/elasticsearch-reload.py
+++ src/main/python/elasticsearch-reload.py
@@ -34,25 +34,64 @@ def get_settings(args):
     raise_with_answer_text(r, 'Could not get settings!')
     return r.json
 
-def get_updates(settings):
-    updates = {}
+def collect_node_local_inconsistencies(settings):
+    local_inconsistencies = set()
 
-    cluster_time = settings['settings']['cluster']['timestamp']
     cluster_ts = settings['settings']['cluster']['timestamp_in_millis']
 
     for node_id, node in settings['settings']['nodes'].iteritems():
         file_ts = node['file_timestamp_in_millis']
-        file_time = node['file_timestamp']
         for key, setting in node['inconsistencies'].iteritems():
             if not setting['_updatable']:
                 continue # not updatable
             if file_ts <= cluster_ts:
                 continue # file is older
-            update = updates.setdefault(key, {})
-            update['_effective'] = { 'value': setting['effective'], 'ts': cluster_ts, 'time': cluster_time }
-            update[node_id] = { 'value': setting['desired'], 'ts': file_ts, 'time': file_time }
+            local_inconsistencies.add(key)
+
+    return local_inconsistencies
+
+def get_updates(settings, node_local_inconsistencies):
+    updates = {}
+
+    cluster_ts = settings['settings']['cluster']['timestamp_in_millis']
+    cluster_time = settings['settings']['cluster']['timestamp']
+
+    oldest_file_ts = None
+    oldest_file_time = None
+    for node_id, node in settings['settings']['nodes'].iteritems():
+        file_ts = node['file_timestamp_in_millis']
+        file_time = node['file_timestamp']
+        if file_ts > oldest_file_ts:
+            oldest_file_ts = file_ts
+            oldest_file_time = file_time
+
+    for key in node_local_inconsistencies:
+        update = updates[key] = {}
+        from_cluster = key in settings['settings']['cluster']['effective']
+        consistent   = key in settings['settings']['consistencies']['effective']
+        inconsistent = key in settings['settings']['inconsistencies']['effective']
+        inconsistencies = {}
+        for node_id, node in settings['settings']['nodes'].iteritems():
+            file_ts = node['file_timestamp_in_millis']
+            file_time = node['file_timestamp']
+            update[node_id] = { 'value': node['desired'].get(key), 'ts': file_ts, 'time': file_time }
+            if inconsistent:
+                inconsistencies[node_id] = { 'value': node['effective'].get(key), 'ts': file_ts, 'time': file_time }
+        if from_cluster:
+            update['_effective'] = { 'from': 'cluster', 'value': settings['settings']['cluster']['effective'][key], 'ts': cluster_ts, 'time': cluster_time }
+        elif consistent:
+            update['_effective'] = { 'from': 'nodes', 'value': settings['settings']['consistencies']['effective'][key], 'ts': oldest_file_ts, 'time': oldest_file_time }
+        elif inconsistent:
+            update['_effective'] = { 'from': 'nodes', 'inconsistent': inconsistencies }
+        else:
+            update['_effective'] = { 'from': None, 'value': None, 'ts': 0, 'time': None }
 
     return updates
+
+def format_multiple_values(source):
+    sorted_source = sorted(source.iteritems(), key=lambda o: o[1]['ts'])
+    explanation = ['%s:[%s]%s' % (node_id, node['time'], '(none)' if node['value'] is None else node['value']) for node_id, node in sorted_source]
+    return ', '.join(explanation)
 
 def get_update_decisions(updates):
     update_request = {}
@@ -60,14 +99,23 @@ def get_update_decisions(updates):
         effective = nodes.pop('_effective')
         values = set([v['value'] for v in nodes.values()])
         if len(values) > 1:
-            sorted_nodes = sorted(nodes.iteritems(), key=lambda o: o[1]['ts'])
-            explanation = ['%s:[%s]%s' % (node_id, node['time'], '(none)' if node['value'] is None else node['value']) for node_id, node in sorted_nodes]
-            if effective['value'] is None:
-                effective['value'] = '(none)'
-            print 'No unanimity in uptodate value for %s: effective:[%s]%s, %s' % (key, effective['time'], effective['value'], ', '.join(explanation))
+            if 'inconsistent' in effective:
+                print 'No unanimity in uptodate value for %s: effective(from %s): %s, desired: %s' % (key, effective['from'], format_multiple_values(effective['inconsistent']), format_multiple_values(nodes))
+            else:
+                if effective['value'] is None:
+                    effective['value'] = '(none)'
+                print 'No unanimity in uptodate value for %s: effective(from %s): [%s]%s, desired: %s' % (key, effective['from'], effective['time'], effective['value'], format_multiple_values(nodes))
             continue
         value = values.pop()
-        print 'Update', key, 'from', effective['value'], 'to', value
+        if effective['from'] is None:
+            effective_str = '(unset)'
+        else:
+            if 'inconsistent' in effective:
+                effective['value'] = format_multiple_values(effective['inconsistent'])
+            if effective['value'] is None:
+                effective['value'] = '(none)'
+            effective_str = '(%s) %s' % (effective['from'], effective.get('value'))
+        print 'Update %s from %s to %s' % (key, effective_str, value)
         update_request[key] = value
     return update_request
 
@@ -88,7 +136,8 @@ def apply_update_decisions(args, update_decisions):
 def main():
     args = parse()
     settings = get_settings(args)
-    updates = get_updates(settings)
+    local_inconsistencies = collect_node_local_inconsistencies(settings)
+    updates = get_updates(settings, local_inconsistencies)
     update_decisions = get_update_decisions(updates)
     if has_update_decisions(update_decisions):
         apply_update_decisions(args, update_decisions)
